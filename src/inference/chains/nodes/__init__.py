103147e58e772b3a7786688fd472cf054864144a
= <=nerdchanii@gmail.com>
2025-12-21 11:13:41 +0000

    refactor(inference): migrate inference pipeline to LangChain runnables

---
+++ src/inference/chains/nodes/__init__.py
@@ -0,0 +1,3 @@
+from .mcq_head_nodes import create_choice_scorer, create_forward, decode_prediction, format_row
+
+__all__ = ["create_choice_scorer", "create_forward", "decode_prediction", "format_row"]
