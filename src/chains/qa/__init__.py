cdb6b6d53d9ca55782c832a504977f6608d638cf
nerdchanii <nerdchanii@gmail.com>
2026-01-04 22:20:30 +0900

    feat:  CoT with structured output

--- src/chains/qa/__init__.py
+++ src/chains/qa/__init__.py
@@ -1,3 +1,4 @@
 from chains.qa.chain import build_qa_chain
+from chains.qa.inference.not_think_cot import build_non_think_cot_chain
 
-__all__ = ["build_qa_chain"]
+__all__ = ["build_qa_chain", "build_non_think_cot_chain"]
